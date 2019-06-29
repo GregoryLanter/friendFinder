35e5bcdd15e6c0d78c45184c182f827ebd85cb4a
Greg Lanter <dohnut22.gl@gmail.com>
2019-06-29 09:22:20 -0700

    changes to PORT fro heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require("express");
 let path = require("path");
 
 let app = express();
-let PORT = process.env.port || 8080;
+let PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
